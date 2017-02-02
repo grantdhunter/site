a74afab1fbc9f1d41863f24e3007f2e3b437750d
Grant Hunter <gr@nt-hunter.ca>
2017-02-01 22:13:06 -0700

    start of login endpoint

--- rocket/src/main.rs
+++ rocket/src/main.rs
@@ -4,15 +4,38 @@
 extern crate rocket;
 extern crate models;
 
+use rocket::request::{self, Request, FromRequest};
+use rocket::outcome::Outcome::*;
+use models::models::{UsrSecure};
 
 
+#[derive(Debug)]
+struct Authentication(());
 
+impl<'a, 'r> FromRequest<'a, 'r> for Authentication {
+    type Error = ();
+    fn from_request(req: &'a Request<'r>) -> request::Outcome<Self, ()> {
+        let h = req.headers().get("Authorization").next();
+        println!("{:?}", h);
+        Success(Authentication(()))
+    }
+}
+
+
+#[post("/")]
+fn login(auth: Authentication) -> &'static str {
+    "Login"
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .mount("/login", routes![login])
+        .launch();
 }
